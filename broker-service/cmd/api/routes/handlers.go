37e2e5f07648b731848541acda08c678ac426359
murat <muratnysambaev93@gmail.com>
2024-04-11 21:21:01 +0500

    helpers

--- broker-service/cmd/api/routes/handlers.go
+++ broker-service/cmd/api/routes/handlers.go
@@ -1,27 +1,18 @@
 package routes
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
-
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
-	payload := jsonResponse{
+	payload := JsonResponse{
 		Error:   false,
 		Message: "Hit the broker",
 	}
 
-	out, err := json.MarshalIndent(payload, "", "\t")
+	err := app.writeJSON(w, http.StatusOK, payload)
+
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+		panic(err)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
 }

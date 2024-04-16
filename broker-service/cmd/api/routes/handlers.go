56a2cb478ff8d11415caae58733f6108efadca74
murat <muratnysambaev93@gmail.com>
2024-04-16 08:59:39 +0500

    LogItem endpoint

--- broker-service/cmd/api/routes/handlers.go
+++ broker-service/cmd/api/routes/handlers.go
@@ -11,11 +11,17 @@ import (
 type RequestPayload struct {
 	Action string      `json:"action"`
 	Auth   AuthPayload `json:"auth,omitempty"`
+	Log    LogPayload  `json:"log,omitempty"`
 }
 
 type AuthPayload struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -43,6 +49,8 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 	case "auth":
 		app.Authenticate(w, r, requestPayload.Auth)
+	case "log":
+		app.LogItem(w, r, requestPayload.Log)
 	default:
 		app.errorJSON(w, errors.New("unk action"))
 	}
@@ -94,6 +102,39 @@ func (app *Config) Authenticate(w http.ResponseWriter, r *http.Request, a AuthPa
 	payload.Error = false
 	payload.Message = "Authenticated!"
 	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) LogItem(w http.ResponseWriter, r *http.Request, logEntry LogPayload) {
+	jsonData, _ := json.MarshalIndent(logEntry, "", "\t")
+	logServiceURL := "http://log-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, fmt.Errorf("unexpected status code: %d", response.StatusCode))
+		return
+	}
+
+	var payload JsonResponse
+	payload.Error = false
+	payload.Message = "Logged!"
 
 	app.writeJSON(w, http.StatusAccepted, payload)
 }
